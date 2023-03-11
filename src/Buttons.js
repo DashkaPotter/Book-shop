5d18362117384d6fb68314c69d1c2b27c2a5626a
DashkaPotter <121686163+DashkaPotter@users.noreply.github.com>
2023-03-11 17:56:30 +0300

    Update Buttons.js

--- src/Buttons.js
+++ src/Buttons.js
@@ -1,11 +1,11 @@
 
-function Buttons({filteredBooks}){
+function Buttons({filteredBooks,data,setBooks}){
     return(
         <div className="cont btn">
             <button className="change fantasy" onClick={()=>filteredBooks("fantasy")}>Fantasy</button>
                 <button className="change novel" onClick={()=>filteredBooks("novel")}>Novels</button>
             <button className="change detective" onClick={()=>filteredBooks("detective")}>Detective</button>
-            <button className="change all" onClick={() => window.location.reload(false)}>All</button>
+            <button className="change all" onClick={() => setBooks(data)}>All</button>
         </div>
     )
 }
